60d3af5237085ee75903f3e9fb26c8171adb6902
sebv <seb.vincent@gmail.com>
2015-05-20 11:45:22 -0700

    lets travis use the spec reporter

--- gulpfile.js
+++ gulpfile.js
@@ -3,4 +3,8 @@
 var gulp = require('gulp'),
     boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
-boilerplate({build: 'mobile-json-wire-protocol', jscs: false});
+boilerplate({
+  build: 'mobile-json-wire-protocol',
+  jscs: false,
+  testReporter: process.env.TRAVIS ? 'spec' : 'nyan'
+});
