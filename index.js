e109407404788fed174efa55830771b29a39a73d
Jonathan Lipps <jlipps@gmail.com>
2015-05-22 09:45:07 -0700

    export an 'isSessionCommand' helper method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // transpile:main
 
-import { MJSONWP as MobileJsonWireProtocol,
+import { MJSONWP as MobileJsonWireProtocol, isSessionCommand,
          routeConfiguringFunction } from './lib/mjsonwp';
 import { NO_SESSION_ID_COMMANDS } from './lib/routes';
 import { errors } from './lib/errors';
@@ -8,4 +8,5 @@ import { errors } from './lib/errors';
 export { MobileJsonWireProtocol,
          routeConfiguringFunction,
          errors,
-         NO_SESSION_ID_COMMANDS };
+         NO_SESSION_ID_COMMANDS,
+         isSessionCommand };
