bba0eb34d190a405d24940e1a92128681bb25ba1
Jonathan Lipps <jlipps@gmail.com>
2015-05-23 20:20:48 -0700

    frames spec requires 'id' param not 'frameId' (at least that's what wd does)

--- test/routes-specs.js
+++ test/routes-specs.js
@@ -34,7 +34,7 @@ describe('MJSONWP', () => {
       }
       var hash = shasum.digest('hex').substring(0, 8);
       // Modify the hash whenever the protocol has intentionally been modified.
-      hash.should.equal('0c04ef85');
+      hash.should.equal('7fa2f631');
     });
   });
 
