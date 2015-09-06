d52e26394763f9890440439ddbf352b7725be8d4
sebv <seb.vincent@gmail.com>
2015-09-06 17:38:08 +0800

    exposing all commands

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 import { MJSONWP as MobileJsonWireProtocol, isSessionCommand,
          routeConfiguringFunction } from './lib/mjsonwp';
-import { NO_SESSION_ID_COMMANDS } from './lib/routes';
+import { NO_SESSION_ID_COMMANDS, ALL_COMMANDS } from './lib/routes';
 import { errors, isErrorType, errorFromCode } from './lib/errors';
 
 export { MobileJsonWireProtocol,
@@ -10,5 +10,6 @@ export { MobileJsonWireProtocol,
          errors,
          isErrorType,
          errorFromCode,
+         ALL_COMMANDS,
          NO_SESSION_ID_COMMANDS,
          isSessionCommand };
